54bcf5827837a83910737e79ed6c28f5bb80ab9d
Jay Oswald <jay@oswald.net.au>
2019-11-18 17:07:00 +1100

    rebuilding

--- eslint/dist/index.js
+++ eslint/dist/index.js
@@ -4486,10 +4486,24 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
+
+  console.log("CONTENST: " + JSON.stringify(get_contents()));
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+
+function get_contents(github){
+  console.log("GETTING CONTENTS");
+  return github.git.getTree({
+    owner: github.context.payload.repository.organization,
+    repo: github.context.payload.repository.name,
+    tree_sha: github.context.payload.head_commit.tree_id,
+    recursive: true
+  })
 }
 
 /***/ }),
