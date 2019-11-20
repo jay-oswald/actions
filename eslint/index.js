ca1991f299189bd229c9bbb6e6b268db4d4f807d
Jay Oswald <jay@oswald.net.au>
2019-11-20 14:58:35 +1100

    removing hopefully unceessary items

--- eslint/index.js
+++ eslint/index.js
@@ -1,5 +1,3 @@
-const request = require('./request');
-
 const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN, GITHUB_WORKSPACE } = process.env;
 const event = require(GITHUB_EVENT_PATH);
 const { repository } = event;
@@ -8,34 +6,8 @@ const {
 } = repository;
 const { name: repo } = repository;
 
-const checkName = 'ESLint check';
-
-const headers = {
-  'Content-Type': 'application/json',
-  Accept: 'application/vnd.github.antiope-preview+json',
-  Authorization: `Bearer ${GITHUB_TOKEN}`,
-  'User-Agent': 'eslint-action'
-};
-
-
 run().catch(exitWithError);
 
-async function createCheck() {
-  const body = {
-    name: checkName,
-    head_sha: GITHUB_SHA,
-    status: 'in_progress',
-    started_at: new Date()
-  };
-
-  const { data } = await request(`https://api.github.com/repos/${owner}/${repo}/check-runs`, {
-    method: 'POST',
-    headers,
-    body
-  });
-  const { id } = data;
-  return id;
-}
 
 function eslint() {
   const eslint = require('eslint');
@@ -73,24 +45,6 @@ function eslint() {
     }
   }
 }
-
-async function updateCheck(id, conclusion, output) {
-  const body = {
-    name: checkName,
-    head_sha: GITHUB_SHA,
-    status: 'completed',
-    completed_at: new Date(),
-    conclusion,
-    output
-  };
-
-  await request(`https://api.github.com/repos/${owner}/${repo}/check-runs/${id}`, {
-    method: 'PATCH',
-    headers,
-    body
-  })
-}
-
 function exitWithError(err) {
   console.error('Error', err.stack)
   if (err.data) {
@@ -100,16 +54,13 @@ function exitWithError(err) {
 }
 
 async function run() {
-  const id = await createCheck();
   try {
     const { conclusion, output } = eslint();
     console.log(output.summary);
-    await updateCheck(id, conclusion, output);
     if (conclusion === 'failure') {
       process.exit(78)
     }
   } catch (err) {
-    await updateCheck(id, 'failure');
     exitWithError(err)
   }
 }
